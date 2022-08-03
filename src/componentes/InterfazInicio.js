0324257f5f1f524c9ecd2c8ae43df127bf3b037d
Marroquin2897 <57332287+Marroquin2897@users.noreply.github.com>
2022-08-02 21:04:18 -0500

    Update InterfazInicio.js
    Cambie el titulo de interfaz inicio

--- src/componentes/InterfazInicio.js
+++ src/componentes/InterfazInicio.js
@@ -24,7 +24,7 @@ const InterfazInicio = () => {
           <title>Inicio</title>
       </Helmet>
     <Header>
-        <Titulo> N O T I C I A S </Titulo> 
+        <Titulo> AQUI VA LA INTERFAZ DE INICIO </Titulo> 
         
    
     </Header>
