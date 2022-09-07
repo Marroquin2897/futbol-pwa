d7238b3204ac00d72cc4bd4feddf52f351520b05
Marroquin2897 <57332287+Marroquin2897@users.noreply.github.com>
2022-09-06 20:35:41 -0500

    agregar jugadores funcionando

--- src/componentes/RegistrarJugador.js
+++ src/componentes/RegistrarJugador.js
@@ -5,9 +5,10 @@ import { faFutbol } from '@fortawesome/free-solid-svg-icons';
 import { IconoInicio } from './../elementos/ElementosFormularios';
 import {ContenedorBoton, Formulario, Input } from './../elementos/ElementosFormularios';
 import Boton from './../elementos/Boton';
-import Select from 'react-select';
-
+import Alerta from './../elementos/Alerta';
 import ContenedorDiv from './../elementos/ContenedorDiv';
+import {db} from './../firebase/firebaseConfig';
+import {collection, addDoc} from 'firebase/firestore';
 
 const RegistrarJugador = () => {
     const [nombre, cambiarNombre] = useState('');
@@ -17,6 +18,8 @@ const RegistrarJugador = () => {
     const [curp, cambiarCurp] = useState('');
     const [boleta, cambiarBoleta] = useState('');
     const [semestre, cambiarSemestre] = useState('');
+    const[estadoAlerta,cambiarEdoAlerta] = useState(false);
+    const[alerta,cambiarAlerta] = useState({});
 
     const handleChange = (e) => {
         switch(e.target.name){
@@ -45,7 +48,39 @@ const RegistrarJugador = () => {
                 break;
         }
     }
-   
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if(nombre === '' || apellidos === '' || fechanac ==='' || nss === '' || curp ===''|| boleta ==='' || semestre ===''){
+            cambiarEdoAlerta(true);
+            cambiarAlerta({
+                tipo: 'error',
+                mensaje:'Completa todos los campos'
+            });
+            return;
+        }
+        try{
+            await addDoc(collection(db,'jugadores'),{
+                nombre:nombre,
+                apellidos: apellidos,
+                fechanac: fechanac,
+                nss: nss,
+                curp: curp,
+                boleta: boleta,
+                semestre: semestre
+            });
+        } catch(error){
+            console.log('Error al registrar jugador');
+        }
+        cambiarNombre('');
+        cambiarApellidos('');
+        cambiarFechaNac('');
+        cambiarNss('');
+        cambiarCurp('');
+        cambiarBoleta('');
+        cambiarSemestre('');
+    }
+
     return ( 
         <>
         <Helmet>
@@ -54,51 +89,63 @@ const RegistrarJugador = () => {
         <IconoInicio icon={faFutbol}/>
         <Titulo> Registrar Jugador </Titulo> 
         <ContenedorDiv>
-            <Formulario >
+            <Formulario onSubmit={handleSubmit}>
             
                 <Input
                     type='text'
                     name='nombre'
-                    placeholder='Nombre del alumno(a)'         
+                    value={nombre}
+                    placeholder='Nombre del alumno(a)'  
+                    onChange={handleChange}       
                 />
                 <Input
                     type='text'
                     name='apellidos'
-                    placeholder='Apellidos del alumno(a)'          
+                    value={apellidos}
+                    placeholder='Apellidos del alumno(a)'    
+                    onChange={handleChange}        
                 />
                 <Input
                     type='date'
-                    name='fechanac'          
+                    name='fechanac'  
+                    value={fechanac}     
+                    onChange={handleChange}   
                 />
                     
                 <Input
                     type='text'
                     name='nss'
-                    placeholder='Número de seguro social del alumno(a)' 
+                    placeholder='Número de seguro social del alumno(a)'
+                    value={nss}     
+                    onChange={handleChange} 
                 />
                 <Input
                     type='text'
                     name='curp'
                     placeholder='CURP del alumno(a)' 
+                    value={curp}     
+                    onChange={handleChange} 
                 />
                 <Input
                     type='text'
                     name='boleta'
                     placeholder='Boleta del alumno(a)' 
+                    value={boleta}     
+                    onChange={handleChange} 
                 />
                 <Input
                     type='text'
                     name='semestre'
                     placeholder='Semestre del alumno(a)' 
+                    value={semestre}     
+                    onChange={handleChange} 
                 />   
                 <ContenedorBoton>
                 <Boton as="button" type="submit"> Registrar Jugador </Boton>  
                 </ContenedorBoton>
 
             </Formulario>
-        </ContenedorDiv>
-       
-       
+        </ContenedorDiv>    
     </>
      );
 }
