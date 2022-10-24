31ac465a2d083d096a7f2c3c7e9e60463ceff20f
Marroquin2897 <57332287+Marroquin2897@users.noreply.github.com>
2022-10-24 14:55:15 -0500

    update rol juegos

--- src/componentes/RolJuegos.js
+++ src/componentes/RolJuegos.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import {Titulo} from '../elementos/Header';
+import ContenedorDiv from './../elementos/ContenedorDiv';
+import {ContenedorBoton, Formulario, Input } from './../elementos/ElementosFormularios';
+import Boton from './../elementos/Boton';
+import { Link } from 'react-router-dom';
 const RolJuegos = () => {
+    
+
+    const CalularLigaNumEquiposPar = ( ) => {
+
+    }
+
+    const MostrarPartidos = () => {
+
+    }
+
+    const CalcularLiga = () => {
+
+    }
     return ( 
             <>
             <Helmet>
             <title> Rol de Juegos</title>
             </Helmet>
             <Titulo> Juegos Programados </Titulo> 
+
+            <ContenedorDiv>
+            <Formulario action=""  >
+                
+                <Input
+                    type='text'
+                    name='numequipos'
+                    placeholder='Ingresa no. equipos'
+                          
+                /> 
+                
+                <ContenedorBoton>
+                    <Boton as="button" type="submit"> Generar Juegos </Boton>
+                    <Boton as={Link} to="/inicio">Regresar</Boton>  
+                </ContenedorBoton>
+            </Formulario>
+        </ContenedorDiv>
             </>
      );
 }
  
-export default RolJuegos;
+export default RolJuegos;  
